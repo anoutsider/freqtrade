734858f9e638920c15ab6a0587377acb45725c9d
anoutsider <amrenator@gmail.com>
2022-04-11 12:54:14 +0400

    Added support for bybit futures

--- freqtrade/exchange/bybit.py
+++ freqtrade/exchange/bybit.py
@@ -1,10 +1,15 @@
 """ Bybit exchange subclass """
 import logging
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 from freqtrade.enums import MarginMode, TradingMode
 from freqtrade.exchange import Exchange
 
+from freqtrade.exchange.common import retrier
+
+import ccxt
+
+from freqtrade.exceptions import DDosProtection, OperationalException, TemporaryError
 
 logger = logging.getLogger(__name__)
 
@@ -28,4 +33,31 @@ class Bybit(Exchange):
         # TradingMode.SPOT always supported and not required in this list
         # (TradingMode.FUTURES, MarginMode.CROSS),
         # (TradingMode.FUTURES, MarginMode.ISOLATED)
+        (TradingMode.FUTURES, MarginMode.ISOLATED)
     ]
+
+    @retrier
+    def _set_leverage(
+        self,
+        leverage: float,
+        pair: Optional[str] = None,
+        trading_mode: Optional[TradingMode] = None
+    ):
+        """
+        Set's the leverage before making a trade, in order to not
+        have the same leverage on every trade
+        """
+        trading_mode = trading_mode or self.trading_mode
+
+        if self._config['dry_run'] or trading_mode != TradingMode.FUTURES:
+            return
+
+        try:
+            self._api.set_leverage(symbol=pair, leverage=round(leverage))
+        except ccxt.DDoSProtection as e:
+            raise DDosProtection(e) from e
+        except (ccxt.NetworkError, ccxt.ExchangeError) as e:
+            raise TemporaryError(
+                f'Could not set leverage due to {e.__class__.__name__}. Message: {e}') from e
+        except ccxt.BaseError as e:
+            raise OperationalException(e) from e
